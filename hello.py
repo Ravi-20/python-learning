bb3058c4175907a8b9a510476b3c0eeb7be6a542
Ravi-20 <ravi042001@gmail.com>
2026-06-08 15:18:01 +0530

    Initial Python learning repository

---
+++ hello.py
@@ -0,0 +1,9 @@
+#It is a print function that prints anything that is in the paranthesis. 
+print("Hello, world!")
+
+#It is an input function that takes the user's input and stores it in a variable called name.
+name = input("What is your name? ")
+print("Hello, " + name)
+
+#It is an f-string that prints the user's name in a formatted way.
+print(f"Hello, {name}")
