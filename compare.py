97e6d2325c72449d1a5e4a496441ba60aaed4eb0
Ravi-20 <ravi042001@gmail.com>
2026-06-09 09:45:51 +0530

    learnd about != and == conditional statemnt

--- compare.py
+++ compare.py
@@ -1,8 +1,14 @@
 # This program compares two numbers and prints the result.
+# Its a conditional statement program
 x = int(input("What's x? : "))
 y = int(input("what's y?: "))
 
-if x < y or x > y:
+if x != y:
     print("x is not equal to y")
 else:
     print("x is equal to y")
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
