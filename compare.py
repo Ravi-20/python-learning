eb1e864dd175621289bb1dc56800c940e0df11f6
Ravi-20 <ravi042001@gmail.com>
2026-06-09 09:20:36 +0530

    learnd the conditional statement

---
+++ compare.py
@@ -0,0 +1,10 @@
+# This program compares two numbers and prints the result.
+a = int(input("What's a? : "))
+b = int(input("what's b?: "))
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
