e9061e4a8f3b725fc4fbec04db7b2f3b8466ce02
Ravi-20 <ravi042001@gmail.com>
2026-06-11 09:47:47 +0530

    learned about try-except block

--- number.py
+++ number.py
@@ -1,5 +1,9 @@
-try:
-    x = int(input("Enter a number: "))
-    print(f"The number is {x}")
-except ValueError:
-    print("That's not a valid number.")
+#This is an error handling code using try-except block
+while True:
+    try:
+        x = int(input("Enter a number: "))    
+    except ValueError:
+        print("That's not a valid number.")
+    else:
+        break
+print(f"The number is {x}")
