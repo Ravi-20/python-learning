83ff2967c83a68bb3ebb90de8dafc5cdec5b6c52
Ravi-20 <ravi042001@gmail.com>
2026-06-12 11:57:03 +0530

    learned about libraries and different modules in libraries

--- generate.py
+++ generate.py
@@ -1,15 +1,16 @@
 # This code simulates a coin flip and prints the result, using the modules or import
-# import random
+import random
 
-# coin = random.choice(["Heads", "Tails"])
-# print("The coin landed on:", coin)
+coin = random.choice(["Heads", "Tails"])
+print("The coin landed on:", coin)
 
 #This is the from module import statement
-# from random import choice
+from random import choice
 
-# coin = choice(["Heads", "Tails"])
-# print("The coin landed on:", coin)
+coin = choice(["Heads", "Tails"])
+print("The coin landed on:", coin)
 
+#This code generates a random integer between 1 and 10, and then shuffles a list of cards and prints them in random order.
 import random
 
 number = random.randint(1, 10)
