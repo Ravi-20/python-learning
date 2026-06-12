7efbeaf02428e0aca1c1ff7fb0b7ddc7a8871d97
Ravi-20 <ravi042001@gmail.com>
2026-06-12 12:28:43 +0530

    learned about slicing

--- name.py
+++ name.py
@@ -1,4 +1,8 @@
 # This program takes a name as a command-line argument and prints a greeting.
 import sys
 
-print("hello, my name is", sys.argv[1])
+if len(sys.argv) < 2:
+    sys.exit("too less arguments provided.")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
