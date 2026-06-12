9f312894540e39fa38797fe400badd4328ebf0ca
Ravi-20 <ravi042001@gmail.com>
2026-06-12 11:45:40 +0530

    learned about from module

--- generate.py
+++ generate.py
@@ -1,5 +1,11 @@
 # This code simulates a coin flip and prints the result, using the modules or import
-import random
+# import random
 
-coin = random.choice(["Heads", "Tails"])
+# coin = random.choice(["Heads", "Tails"])
+# print("The coin landed on:", coin)
+
+#This is the from module import statement
+from random import choice
+
+coin = choice(["Heads", "Tails"])
 print("The coin landed on:", coin)
