eb9f1d6dce7f78a1d3f47603a73bba4c3a317050
Ravi-20 <ravi042001@gmail.com>
2026-09-10 22:41:18 +0530

    Day-2 of learning

--- practice.py
+++ practice.py
@@ -1,1 +1,3 @@
-print("Twinkle Twinkle little star,\n How I wonder what you are!, \n Up above the world so high,\n Like a diamond in the sky.\n Twinkle Twinkle little star,\n How I wonder what you are!")
+a = int(input("Enter a number: "))
+b = a ** 2
+print(b)
